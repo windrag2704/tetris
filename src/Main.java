b954f48d5af722cae80dac8d5bbd933c416e5ec6
windrag <windrag000@gmail.com>
2018-06-27 06:20:10 +0300

    GUI completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -7,9 +6,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -23,43 +21,90 @@ public class Main extends Application {
         Group root = new Group();
         primaryStage.setTitle("Tetris");
         Scene scene = new Scene(root, 800, 629);
-        scene.setFill(Color.rgb(0x33,0x33,0x33));
+        scene.setFill(Color.rgb(0x33, 0x33, 0x33));
         primaryStage.setScene(scene);
         Tetris tetris = new Tetris();
-        Controller.game = tetris;
+
+        Image leftImg = new Image("left.png");
+        Image rightImg = new Image("right.png");
 
         BorderPane borderPane = new BorderPane();
-        StackPane stackPane = new StackPane();
-        GridPane grid = new GridPane();
+        GridPane right = new GridPane();
+        GridPane left = new GridPane();
+
+        right.prefHeight(600);
+        right.prefWidth(250);
+        right.setBackground(
+                new Background(
+                        new BackgroundImage(
+                                rightImg,
+                                null,
+                                null,
+                                null,
+                                null
+                        )
+                )
+        );
+        right.getColumnConstraints().addAll(new ColumnConstraints(65), new ColumnConstraints(120), new ColumnConstraints(65));
+        right.getRowConstraints().addAll(
+                new RowConstraints(80),
+                new RowConstraints(60),
+                new RowConstraints(330),
+                new RowConstraints(60),
+                new RowConstraints(70)
+        );
+
+        left.setPrefHeight(600);
+        left.setPrefWidth(250);
+        left.setBackground(
+                new Background(
+                        new BackgroundImage(
+                                leftImg,
+                                null,
+                                null,
+                                null,
+                                null
+                        )
+                )
+        );
+        left.getColumnConstraints().addAll(
+                new ColumnConstraints(65),
+                new ColumnConstraints(120),
+                new ColumnConstraints(65)
+        );
+        left.getRowConstraints().addAll(
+                new RowConstraints(100),
+                new RowConstraints(20),
+                new RowConstraints(430)
+        );
 
         Canvas canvas = new Canvas(300, 600);
-        Canvas left = new Canvas(250, 600);
-        Canvas right = new Canvas(250, 600);
-        Canvas nextFigure = new Canvas(120,60);
+        Canvas nextFigure = new Canvas(120, 60);
+        Text score = new Text();
         Text info = new Text();
+
+        right.add(nextFigure, 1, 1);
+        right.add(score, 1, 3);
+        left.add(info, 1, 1);
+
+        score.setWrappingWidth(120);
+        score.setFont(new Font(60));
+        score.setFill(Color.valueOf("#aaaaaa"));
+        score.setTextAlignment(TextAlignment.CENTER);
+
+        info.setWrappingWidth(120);
         info.setFont(new Font(20));
-        info.setFill(Color.rgb(0xaa,0xaa,0xaa));
         info.setTextAlignment(TextAlignment.CENTER);
-        Controller.score = info;
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        GraphicsContext gcLeft = left.getGraphicsContext2D();
-        GraphicsContext gcRight = right.getGraphicsContext2D();
         GraphicsContext gcNext = nextFigure.getGraphicsContext2D();
-        stackPane.getChildren().add(right);
-        stackPane.getChildren().add(grid);
-        grid.setPrefHeight(150);
-        grid.setAlignment(Pos.CENTER);
-        grid.setVgap(400);
-        grid.add(nextFigure,0,0);
-        grid.add(info,0,1);
 
-        gcLeft.setFill(Color.DARKRED);
-        gcLeft.fillRect(0, 0, 250, 600);
-        gcRight.setFill(Color.DARKRED);
-        gcRight.fillRect(0, 0, 250, 600);
+        Controller.game = tetris;
         Controller.gc = gc;
         Controller.gcNext = gcNext;
+        Controller.score = score;
+        Controller.info = info;
+        Controller.refresh();
 
         MenuBar menuBar = new MenuBar();
         Menu menuGame = new Menu("Game");
@@ -67,22 +112,34 @@ public class Main extends Application {
         menuBar.getMenus().addAll(menuGame, menuAbout);
 
         MenuItem startGame = new MenuItem("Start");
+        MenuItem stopGame = new MenuItem("Stop");
+        stopGame.setVisible(false);
+
         menuGame.getItems().add(startGame);
+        menuGame.getItems().add(stopGame);
+
         startGame.setOnAction(event -> {
-            startGame.setDisable(true);
-            tetris.launch();
+            startGame.setVisible(false);
+            stopGame.setVisible(true);
+            tetris.start();
+        });
+
+        stopGame.setOnAction(event -> {
+            startGame.setVisible(true);
+            stopGame.setVisible(false);
+            tetris.stop();
         });
 
         MenuItem restartGame = new MenuItem("Restart");
         menuGame.getItems().add(restartGame);
-        restartGame.setOnAction(event -> tetris.restart() );
+        restartGame.setOnAction(event -> tetris.restart());
 
         MenuItem exitGame = new MenuItem("Exit");
         menuGame.getItems().add(exitGame);
         exitGame.setOnAction(event -> System.exit(0));
 
         borderPane.setLeft(left);
-        borderPane.setRight(stackPane);
+        borderPane.setRight(right);
         borderPane.setCenter(canvas);
         borderPane.setTop(menuBar);
         root.getChildren().add(borderPane);
